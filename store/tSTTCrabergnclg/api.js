25aacd4083e54cd99014cd2bb1a6b61dfb156a9f
Crowdbotics <engineering@crowdbotics.com>
2023-08-25 12:34:00 +0000

    Update store.

---
+++ store/tSTTCrabergnclg/api.js
@@ -0,0 +1,6 @@
+import axios from "axios"
+const tSTTCrabergnclg = axios.create({
+  baseURL: "https://www.dsconnect.com/api",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+export const apiService = {}
